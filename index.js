5d5f227e713e79b445c8e52b165fd059256b348b
Micah Shute <micah.shute@gmail.com>
2017-12-14 00:32:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,21 +10,67 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var price = Math.round(Math.random() * 100);
+ cart.push({[item]: price})
+ console.log(`${item} has been added to your cart.`)
+ return cart
 }
 
 function viewCart() {
-  // write your code here
+  var keys = [];
+  for(var i =0; i < cart.length; i ++){
+    if(Object.keys(cart[i]).length === 0){break}
+    keys.push(Object.keys(cart[i])[0])
+  }
+  var output = keys.length <= 0 ? `Your shopping cart is empty.` : `In your cart, you have `
+  for(var i = 0 ; i < keys.length; i++){
+    if(i !== 0 & i !== keys.length - 1){
+      output += `, `
+    }else if(i === keys.length - 1){
+      output += `, and ` 
+    }
+    var price = getValueForKey(keys[i])
+    output += `${keys[i]} at $${price}`
+    if(i === keys.length - 1){output += `.`}
+  }
+  console.log(output)
+  return output
+}
+
+function getValueForKey(key){
+  var i = 0;
+  while(!cart[i].hasOwnProperty(key)){
+    i++;
+  }
+  return cart[i][key]
 }
 
 function total() {
-  // write your code here
+  var total = 0;
+  for(var i = 0; i < cart.length; i++){
+    total += parseInt(getValueForKey(Object.keys(cart[i])[0]))
+  }
+  return total;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for(var i = 0; i < cart.length; i++){
+    if(cart[i].hasOwnProperty(item)){
+      setCart(slice(cart,))
+    }
+  }
 }
 
 function placeOrder(cardNumber) {
   // write your code here
 }
+
+function getPriceForObj(obj){
+  
+}
+// setCart([{}]);
+addToCart("stuff")
+addToCart("moreStuff")
+addToCart("lastThing")
+viewCart()
+total()
